6f47fd95c040d1e3c00446ddb0eaded7d4010a50
Richard George <rcgeorge23@gmail.com>
2017-06-20 16:30:06 +0100

    made the body column a clob. also updated the domain

--- scripts/restart-latest.sh
+++ scripts/restart-latest.sh
@@ -7,6 +7,6 @@ sudo docker pull dockernovinet/smtp-mailer-brochureware
 sudo docker pull dockernovinet/smtp-mailer
 sudo docker pull jwilder/nginx-proxy
 
-sudo docker run -d --restart unless-stopped -e VIRTUAL_HOST=fakesmtp-brochureware.novinet.co.uk -e FAKE_SMTP_HOST=fakesmtp.novinet.co.uk -e FAKE_SMTP_PORT=25 -e VIRTUAL_PORT=9090 -e SERVER_PORT=9090 --log-driver=syslog -p9090:9090 -t dockernovinet/smtp-mailer-brochureware
-sudo docker run -d --restart unless-stopped -e VIRTUAL_HOST=fakesmtp.novinet.co.uk -e VIRTUAL_PORT=8080 -e SERVER_PORT=8080 --log-driver=syslog -p8080:8080 -p25:8025 -t dockernovinet/smtp-mailer
+sudo docker run -d --restart unless-stopped -e VIRTUAL_HOST=smtptrap.net -e FAKE_SMTP_HOST=app.smtptrap.net -e FAKE_SMTP_PORT=25 -e VIRTUAL_PORT=9090 -e SERVER_PORT=9090 --log-driver=syslog -p9090:9090 -t dockernovinet/smtp-mailer-brochureware
+sudo docker run -d --restart unless-stopped -e VIRTUAL_HOST=app.smtptrap.net -e VIRTUAL_PORT=8080 -e SERVER_PORT=8080 --log-driver=syslog -p8080:8080 -p25:8025 -t dockernovinet/smtp-mailer
 sudo docker run -d --restart unless-stopped -p80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
--- src/main/java/uk/co/novinet/smtpmailer/model/SmtpMessage.java
+++ src/main/java/uk/co/novinet/smtpmailer/model/SmtpMessage.java
@@ -2,10 +2,12 @@ package uk.co.novinet.smtpmailer.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
@@ -19,8 +21,15 @@ public class SmtpMessage
 	private String fromAddress;
 	private String toAddress;
 	private String subject;
+	
+	@Column(length=1000000)
+	@Lob
 	private String htmlBody;
+	
+	@Column(length=1000000)
+	@Lob
 	private String plainBody;
+	
 	private Date sentDate;
 	
 	@ManyToOne
